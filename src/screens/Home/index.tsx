13f9dc9934d44064bdd18b948fb10df46e286333
thegabrielrodrigues <gabrielrofreitas.dev@gmail.com>
2023-05-17 14:04:32 -0300

    formulário de adicionar participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,23 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+  function handleAddParticipant() {}
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput style={styles.input} placeholder="Nome do participante" placeholderTextColor="#6B6B6B" />
+
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,30 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+  form: {
+    marginTop: 36,
+    marginBottom: 42,
+    flexDirection: 'row',
+  },
+  input: {
+    height: 56,
+    padding: 16,
+    borderRadius: 5,
+    marginRight: 10,
+    backgroundColor: '#1F1E25',
+    color: '#FFFFFF',
+    flex: 1,
+  },
+  button: {
+    height: 56,
+    width: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  },
 });
