789ca4758d03fd96e0737044545c4265e441292e
thegabrielrodrigues <gabrielrofreitas.dev@gmail.com>
2023-05-17 13:36:22 -0300

    criação da Home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: '#131016',
+    flex: 1,
+  },
+  eventName: {
+    marginTop: 48,
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  },
+});
